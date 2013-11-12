bd1ea131b4d9032d3123edb005e3ca8972e24f4a
Brian Johnsen <github@brianjohnsen.dk>
2013-11-12 16:01:59 +0100

    opdateret grails version.

--- spocktest/grails-app/domain/spocktest/MyDomain.groovy
+++ spocktest/grails-app/domain/spocktest/MyDomain.groovy
@@ -2,7 +2,7 @@ package spocktest
 
 import groovy.transform.ToString
 
-@ToString(includeNames = true, includeFields = true, excludes = 'metaClass,class,version')
+@ToString(includeNames = true, includePackage = false, includeFields = true, excludes = 'metaClass,class,version')
 class MyDomain {
 
     String card
